8116ad6267f2db3390824e8e5f6114dc30470e07
jing.zhang-4 <a996336999>
2019-06-03 09:54:40 +0800

    适配器模式 提交代码

--- src/com/heu/adapter/ObjectAdapter.java
+++ src/com/heu/adapter/ObjectAdapter.java
@@ -7,6 +7,11 @@ public class ObjectAdapter implements Target{
 
     private Adaptee adaptee;
 
+    /**
+     * 无参构造器，没法调用Adaptee中的method1 方法
+     */
+    public ObjectAdapter() {}
+
     public ObjectAdapter(Adaptee adaptee) {
         this.adaptee = adaptee;
     }
@@ -26,7 +31,10 @@ public class ObjectAdapter implements Target{
 class ObjectAdapterTest {
     public static void main(String[] args) {
         Adaptee adaptee = new Adaptee();
-        ObjectAdapter objectAdapter = new ObjectAdapter(adaptee);
+        ObjectAdapter objectAdapter1 = new ObjectAdapter(adaptee);
+        objectAdapter1.method1();
+        objectAdapter1.method2();
+        ObjectAdapter objectAdapter = new ObjectAdapter();
         objectAdapter.method1();
         objectAdapter.method2();
     }
