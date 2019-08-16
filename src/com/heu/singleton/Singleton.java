9bc920776bba4f2758691921d5d95577292cde29
jing.zhang-4 <a996336999>
2019-08-16 16:04:50 +0800

    单例模式 提交代码

--- src/com/heu/singleton/Singleton.java
+++ src/com/heu/singleton/Singleton.java
@@ -123,6 +123,9 @@ class LazySingletonUpgrade {
     }
 }
 
+/**
+ * 双重检查锁定(Double-Check Locking)
+ */
 class LazySingletonDoubleCheck {
     private volatile static LazySingletonDoubleCheck instance = null;
     private LazySingletonDoubleCheck() {}
@@ -142,8 +145,13 @@ class LazySingletonDoubleCheck {
     }
 }
 
+/**
+ * 静态(static)内部类
+ * 由于静态单例对象没有作为Singleton的成员变量直接实例化，因此类加载时不会实例化Singleton，
+ * 第一次调用getInstance()时将加载内部类HolderClass，在该内部类中定义了一个static类型的变量instance，
+ * 此时会首先初始化这个成员变量，由Java虚拟机来保证其线程安全性，确保该成员变量只能初始化一次。
+ */
 class IoDHSingleton {
-
     private  IoDHSingleton() {}
 
     //静态(static)内部类
